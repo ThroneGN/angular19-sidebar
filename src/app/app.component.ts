c2fd610f0e55f38a305ce743bf63ab7971f06335
ThroneGN <grandhonor1@gmail.com>
2025-03-01 13:03:36 -0500

    Desarrollo de un sidebar en Angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+//? Angular Material
+import { MatToolbar } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { CustomSidenavComponent } from "./components/custom-sidenav/custom-sidenav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbar,
+    MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular19-dashboard';
+  title = 'angular19-sidebar';
+
+  collapsed = signal(false);
+
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px');
 }
